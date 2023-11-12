edff084710d0000bef8a7271dafe2e73244b9760
WeihungChen <larry980808@gmail.com>
2023-11-13 00:22:29 +0800

    Add Fillout page

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ async function ImgClicked(event)
         }
     }
 
-    console.log(serverUrl);
     var result = await fetchPost(serverUrl + '/imgcheck', content, "application/json");
     console.log(result);
 
